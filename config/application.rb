b43768f1979fa6ac24c211ebb36daccda3f6fe3a
Duong <yeunuthongminh@gmail.com>
2017-04-04 17:11:02 +0700

    Init i18n-js

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module HelloWorld
@@ -11,5 +11,7 @@ module HelloWorld
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**",
+      "*.{rb,yml}")]
   end
 end
